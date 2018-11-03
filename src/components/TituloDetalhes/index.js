8ceb4ad16bd808154174a46b42ae1431c22493e4
Geovani José <geovani-lol@hotmail.com>
2018-11-03 01:13:09 -0300

    Implementar header da tela de detalhes

--- src/components/TituloDetalhes/index.js
+++ src/components/TituloDetalhes/index.js
@@ -39,25 +39,51 @@ export default class TituloDetalhe extends Component {
     }
   }
 
-  renderTela = (titulo) => (
-    <div className="titulo-info">
-      <h1>{titulo.name}</h1>
-      <img className="titulo-info__imagem" src={titulo.imageUrl} alt=''></img>
-      <p>{titulo.desc}</p>
-      <p>{
-        titulo.history &&
-        titulo.history.map(hist => hist)
-      }</p>
-      <span>{titulo.day}</span>
-    </div>
-  )
+  loadDetalhe = () => {
+    const { titulo } = this.state
+    return (
+      <div className="detalhe-container">
+        <div className="detalhe-header" style={{
+          background: 'linear-gradient(to top, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.1), transparent), url('
+            + titulo.imageUrl + ') center -30px no-repeat',
+          backgroundSize: 'cover'
+        }}>
+          <h1 className="detalhe-header__title">{titulo.name}</h1>
+          <h2 className="detalhe-header__sub">{titulo.day}</h2>
+        </div>
+      </div>
+    )
+  }
 
   render() {
     const { titulo } = this.state
 
     return (
-      (titulo.name && this.renderTela(titulo))
-      || <Loading />
+      (titulo.name && this.loadDetalhe())
+      || <div></div>
     )
   }
-}    
+}
+
+/*
+<div className="titulo-info">
+        <h1>{titulo.name}</h1>
+        <img className="titulo-info__imagem" src={titulo.imageUrl} alt=''></img>
+        <p>{titulo.desc}</p>
+        <p>{
+          titulo.history &&
+          titulo.history.map(hist => hist)
+        }</p>
+        <span>{titulo.day}</span>
+      </div>
+
+<div className="detalhe-container">
+        <div className="detalhe-header">
+          <img className="detalhe-header__imagem" style={{ background:
+            'linear-gradient(transparent, rgba(0, 0, 0, 0.4)), url('
+            + titulo.imageUrl + ') no-repeat' }} alt=''></img>
+          <h1 className="detalhe-header__title">{titulo.name}</h1>
+          <h2 className="detalhe-header__sub">{titulo.day}</h2>
+        </div>
+      </div>
+*/
