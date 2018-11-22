9b4aca917fbe780f5f960f705e61058ce0e8e31d
Geovani José <geovani-lol@hotmail.com>
2018-11-22 17:23:49 -0200

    Adicionar ordenação dos títulos pela data na Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -28,12 +28,22 @@ export default class Home extends Component {
 
       document.querySelector('#nprogress .bar').style.backgroundColor = "#00b46b";
       NProgress.done()
+
+      this.titulosDoDia()
+      
     } catch (err) {
       console.log(err)
 
       document.querySelector('#nprogress .bar').style.backgroundColor = "#f00";
       NProgress.done()
     }
+  }
+
+  titulosDoDia = () => {
+    const datesEqualToday = this.state.titulos.filter(titulo => new Date(`${titulo.day}-2018`).toLocaleDateString() === new Date('11-27-2018').toLocaleDateString())
+
+    this.setState({ titulos: datesEqualToday })
+    console.log(datesEqualToday)
   }
 
   render() {
